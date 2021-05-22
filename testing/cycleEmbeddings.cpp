732b6f13bec24f43c5c2042ed115eb91ab79bd9f
Nelson Penn <nelsonapenn@protonmail.com>
2021-05-22 16:13:57 -0400

    Touched up spacing a bit

--- testing/cycleEmbeddings.cpp
+++ testing/cycleEmbeddings.cpp
@@ -22,38 +22,40 @@ int main()
   std::cout<<A; //redefined stream insertion for Matrix class
   for(int i=3;i<=5;i++)
   {
-    mpf_class x=cycleEmbeddings(i, A);
-    std::cout<<"i = "<<i<<": x = "<<x<<"\n";
+    mpf_class x = cycleEmbeddings(i, A);
+    std::cout << "i = " << i << ": x = " << x << "\n";
   }
+
   return 0;
 }
+
 mpf_class cycleEmbeddings(int length, Matrix<mpf_class> A)  // returns the number of embeddings of a length-cycle into graph with incidence matrix A
 {
-  mpf_class output=0;
-  mpf_class one=1;
+  mpf_class output = 0;
+  mpf_class one = 1;
   switch(length)  // handles the arbirtrary formulas for each length of cycle as per the Evaluation of Matrix Expressions... master project
   {
     case 3:
       {
-        output=Matrix<mpf_class>::tr(A)*one/6;
+        output = Matrix<mpf_class>::tr(A) * one / 6;
         break;
       }
     case 4:
       {
-        Matrix<mpf_class> A2=A*A;
-        Matrix<mpf_class> A4=A2*A2;
-        Matrix<mpf_class> inner=Matrix<mpf_class>::diag(A2);
-        Matrix<mpf_class> h=A.hadamard(A);
+        Matrix<mpf_class> A2 = A*A;
+        Matrix<mpf_class> A4 = A2*A2;
+        Matrix<mpf_class> inner = Matrix<mpf_class>::diag(A2);
+        Matrix<mpf_class> h = A.hadamard(A);
 
-        output=Matrix<mpf_class>::tr(A4)*one/8-Matrix<mpf_class>::tr(inner.hadamard(inner))*one/4+Matrix<mpf_class>::tr(h*h)*one/8;
+        output = Matrix<mpf_class>::tr(A4) * one / 8 - Matrix<mpf_class>::tr(inner.hadamard(inner)) * one / 4 + Matrix<mpf_class>::tr(h*h) * one / 8;
         break;
       }
     case 5:
       {
-      Matrix<mpf_class> A2=A*A;
-      Matrix<mpf_class> A3=A2*A;
-      Matrix<mpf_class> A5=A3*A2;
-      output=Matrix<mpf_class>::tr(A5)*one/10-Matrix<mpf_class>::tr(Matrix<mpf_class>::diag(A2).hadamard(Matrix<mpf_class>::diag(A3)))*one/2+Matrix<mpf_class>::tr(A2.hadamard(A3))*one/2;
+      Matrix<mpf_class> A2 = A*A;
+      Matrix<mpf_class> A3 = A2*A;
+      Matrix<mpf_class> A5 = A3*A2;
+      output=Matrix<mpf_class>::tr(A5) * one / 10 - Matrix<mpf_class>::tr(Matrix<mpf_class>::diag(A2).hadamard(Matrix<mpf_class>::diag(A3))) * one / 2 + Matrix<mpf_class>::tr(A2.hadamard(A3)) * one / 2;
       break;
       }
     case 6:
--- testing/main.cpp
+++ testing/main.cpp
@@ -7,33 +7,33 @@
 int main()
 {
   int m=0;
-  std::cout<<"Enter size of matrix: \n";
-  std::cin>>m;
-  int degree=0;
-  std::cout<<"Enter desired degree of polynomial:\n";
-  std::cin>>degree;
+  std::cout << "Enter size of matrix: \n";
+  std::cin >> m;
+  int degree = 0;
+  std::cout << "Enter desired degree of polynomial:\n";
+  std::cin >> degree;
   //Large example, all 1`s except for diagonal
-  Poly<mpz_class> mPoly=(mpz_class)(m);
-  Matrix<Poly<mpz_class>> I=Matrix<Poly<mpz_class>>::I(m);
-  Matrix<Poly<mpz_class>> A=Matrix<Poly<mpz_class>>(m,m,(mpz_class)(1));
-  A=A-I;
-  Poly<mpz_class> LHS=(mpz_class)(0);
-  Matrix<Poly<mpz_class>> matrixPower=I;
-  for(int i=0;i<=degree;i++)
+  Poly<mpz_class> mPoly = (mpz_class)(m);
+  Matrix<Poly<mpz_class>> I = Matrix<Poly<mpz_class>>::I(m);
+  Matrix<Poly<mpz_class>> A = Matrix<Poly<mpz_class>>(m, m, (mpz_class)(1));
+  A = A - I;
+  Poly<mpz_class> LHS = (mpz_class)(0);
+  Matrix<Poly<mpz_class>> matrixPower = I;
+  for(int i = 0; i <= degree; i++)
   {
-    Poly<mpz_class> x(i,(mpz_class)(0));
-    x.set(i,(mpz_class)(1));
-    LHS=LHS+Matrix<Poly<mpz_class>>::tr(matrixPower)*x;
-    matrixPower=matrixPower*A;
+    Poly<mpz_class> x(i, (mpz_class)(0));
+    x.set(i, (mpz_class)(1));
+    LHS = LHS + Matrix<Poly<mpz_class>>::tr(matrixPower) * x;
+    matrixPower = matrixPower * A;
   }
-  std::cout<<"Large example: \n";
-  std::cout<<"LHS: "<<LHS;
-  Poly<mpz_class> z(1,(mpz_class)(0));
-  z.set(1,(mpz_class)(1));
+  std::cout << "Large example: \n";
+  std::cout << "LHS: " << LHS;
+  Poly<mpz_class> z(1, (mpz_class)(0));
+  z.set(1, (mpz_class)(1));
   
-  Poly<mpz_class> F=Matrix<Poly<mpz_class>>::det(I-(A*z));
-  Poly<mpz_class> generatingFunction=mPoly-Poly<mpz_class>::TaylorSeries(z*F.prime(),F,degree);
-  std::cout<<"RHS: "<<generatingFunction;
+  Poly<mpz_class> F = Matrix<Poly<mpz_class>>::det(I - (A * z));
+  Poly<mpz_class> generatingFunction = mPoly - Poly<mpz_class>::TaylorSeries(z * F.prime(), F, degree);
+  std::cout << "RHS: " << generatingFunction;
   
   return 0;
 }
